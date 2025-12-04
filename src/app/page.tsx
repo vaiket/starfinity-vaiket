c9646f1961e4d96a655de0df363cc6a34cf0aabc
Bikas Sahu <astramizen@gmail.com>
2025-12-04 05:35:45 +0530

    Initial project push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,13 @@ import FAQ from "./(home)/components/FAQ";
 import Footer from "./(home)/components/Footer";
 import SecureFundingProcess from "./(home)/components/SecureFundingProcess";
 import ContactCTA from "./(home)/components/ContactCTA";
-
+import MaintenancePopup from "./(home)/components/MaintenancePopup";
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <MaintenancePopup />
       <Features />
       <Pricing />
       <Testimonials />
